df873a2b665dccf6ca41b6096e3bfb16f1d6fbaf
ashquie <99309014+ashquie@users.noreply.github.com>
2022-02-09 12:58:41 +0530

    Different Print Statement
    Different Way to Print Statement

--- Star.java
+++ Star.java
@@ -3,7 +3,13 @@ package math;
 public class Star {
 
 	public static void main(String[] args) {
-		System.out.print("*\n**\n***\n****");
+		System.out.print("*\n**\n***\n****\n");
+		System.out.println("Hello World");
+		System.out.print("Hello World");
+		System.out.print("Hello World");
+		
+
+					
 	}
 
 }
