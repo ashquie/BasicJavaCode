8200172c801139274ede2901a19eac6f2f8eecf6
ashquie <99309014+ashquie@users.noreply.github.com>
2022-02-09 13:18:49 +0530

    Math +,-,*,/
    simple program to operate math.

---
+++ AreaOfRactangle.java
@@ -0,0 +1,28 @@
+package math;
+
+public class AreaOfRactangle {
+
+	public static void main(String[] args) {
+		int lenght = 10;
+		int breath = 20;
+		String Name = "Vivek Chand";
+		//byte 1 byte = 8 bits
+		//short
+		//char
+		//boolean
+		//int
+		//long
+		//float
+		//double
+		int sum = lenght+breath;
+		int sub = lenght-breath;
+		int multi = lenght*breath;
+		int paran = (lenght*breath)/(lenght-breath);
+		System.out.println(sum);
+		System.out.println(sub);
+		System.out.println(multi);
+		System.out.println(paran);
+
+	}
+
+}
